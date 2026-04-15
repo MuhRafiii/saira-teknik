064b0a607930f9e26e010475b694e64e69d91086
M Rafi <mrafi0603@gmail.com>
2026-04-15 17:56:33 +0700

    real final

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
     server: {
         host: "0.0.0.0", // menerima koneksi dari luar localhost
-        hmr: {
-            host: "https://crackless-unbevelled-lilli.ngrok-free.dev/", // ganti dengan URL ngrok kamu
-        },
     },
     plugins: [
         laravel({
